7897ad662d0daf5bb1b18d31843ffcbe1d9bf24e
GuilhermeAmargo <guilhermecamargosilva211@gmail.com>
2024-09-10 20:08:53 -0300

    feat: new course

---
+++ console.js
@@ -0,0 +1,21 @@
+console.log("Hello, World!");
+
+console.log("Mary Stuart");
+
+console.log("1542");
+
+console.log(1542);
+
+console.log("abc", "def", "ghi");
+
+console.log("Mary Stuart (1542)");
+
+console.log("Mary", "Stuart", "(1542)");
+
+console.log("Mary");
+console.log("Stuart");
+console.log("(1542)");
+
+console.log("Mary" + "Stuart" + "(1542)");
+
+console.log("Mary" + " Stuart" + " (1542)");
